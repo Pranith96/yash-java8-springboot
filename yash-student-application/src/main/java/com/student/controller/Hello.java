8f55773566dde25e80289c9208840289324da1c7
Your Name <you@example.com>
2022-04-25 22:09:14 +0530

    spring boot application start

---
+++ yash-student-application/src/main/java/com/student/controller/Hello.java
@@ -0,0 +1,24 @@
+package com.student.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Hello {
+
+	//http://localhost:8080/hello/data
+	//http://localhost:8080/hello/java
+	
+	@GetMapping("/data")
+	public String hi() {
+		return "Hello Welcome";
+	}
+	
+	@GetMapping("/java")
+	public String display() {
+		return "Hello Welcome to java";
+	}
+
+}
